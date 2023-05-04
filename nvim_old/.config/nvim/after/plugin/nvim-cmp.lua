b6ede2f1fc6070c120e7c0db1166fb44b3ff741b
testusr <smeo@gmx.de>
2023-05-04 20:27:45 +0200

    deprecating vim config

---
+++ nvim_old/.config/nvim/after/plugin/nvim-cmp.lua
@@ -0,0 +1,18 @@
+-- Make sure you setup `cmp` after lsp-zero
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
---
+++ nvim_old/.config/nvim/init.lua
@@ -0,0 +1,8 @@
+require("truehl")
+require("truehl.packer")
+require("truehl.remap")
+require("truehl.set")
+require("truehl.lspsaga")
+require("truehl.telescope")
+require("truehl.comment")
+--print("init.lua")
