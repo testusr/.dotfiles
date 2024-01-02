9c0f91bb89e4a315baa564955344468b3b32ca39
testusr <smeo@gmx.de>
2024-01-02 07:50:44 +0100

    move nvim config to arch for a redo

--- nvim/.config/nvim/lua/user/fugitive.lua
+++
@@ -1,9 +0,0 @@
-local status_ok, comment = pcall(require, "fugitive")
-if not status_ok then
-  return
-end
-
--- Shorten function name
-local keymap = vim.api.nvim_set_keymap
-
-keymap("n", "<leader>gs", vim.cmd.Git);
