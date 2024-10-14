cdda1a7fc04f31b600e61c6f3e373d814a0c8b21
testusr <smeo@gmx.de>
2024-10-14 08:29:26 +0200

    restart lua config

---
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,27 @@
+vim.g.mapleader = " " -- setting the leader key 
+
+local keymap = vim.keymap -- for coniceseness
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>ss", ":source %<CR>", { desc = "Relouad neovim config" }) -- reloade nevom configurations quickly
+
+
+-- increment/decremente numbers 
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment 
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement 
+
+-- window management 
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertivically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally 
+keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make split equal size" }) -- make split window equal size 
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+
+
+-- tabs
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab 
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previos tab" }) -- go to previous tab
+keymap.set("n", "<leader>tg", "<cmd>tabnew %<CR>", { desc = "Open current buffere in new tab" }) -- open current file in new tah
+
+
