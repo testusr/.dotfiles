039b35c51109096fd676023c126b7304f9fe6b82
testusr <smeo@gmx.de>
2025-06-07 04:19:21 +0200

    make init more explicit

--- nvim/.config/nvim/lua/user/plugins/init.lua
+++ nvim/.config/nvim/lua/user/plugins/init.lua
@@ -1,4 +1,20 @@
 return {
-	"nvim-lua/plenary.nvim", -- lua functionas that many plugins use
-	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	-- Plenary: Lua functions used by many plugins
+	{
+		"nvim-lua/plenary.nvim",
+		lazy = true, -- Optional: Load only when required by other plugins
+	},
+	-- Tmux & split window navigation
+	{
+		"christoomey/vim-tmux-navigator",
+		config = function()
+			-- Optional: Add any specific configuration for vim-tmux-navigator
+			vim.g.tmux_navigator_no_mappings = 1 -- Example: Disable default mappings
+			-- Define custom keybindings if needed
+			vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>", { silent = true })
+			vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>", { silent = true })
+			vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>", { silent = true })
+			vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>", { silent = true })
+		end,
+	},
 }
