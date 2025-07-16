6077eed21bebe48996a581df0ddb37c11e93735c
testusr <smeo@gmx.de>
2025-07-16 05:47:23 +0200

    adding <leader>gx to open a browser with the url under the cursor

--- nvim/.config/nvim/lua/user/keymaps.lua
+++ nvim/.config/nvim/lua/user/keymaps.lua
@@ -1,25 +1,49 @@
 vim.g.mapleader = " " -- setting the leader key
 
-local keymap = vim.keymap -- for coniceseness
+local keymap = vim.keymap -- for conciseness
 
+-- Function to open URLs or files in the default application
+local function open_external(file)
+    local sysname = vim.loop.os_uname().sysname:lower()
+    local jobcmd
+    if sysname:match("windows") then
+        jobcmd = {"cmd.exe", "/c", "start", file}
+    elseif sysname:match("darwin") then
+        jobcmd = {"open", file}
+    else
+        jobcmd = {"xdg-open", file}
+    end
+    local job = vim.fn.jobstart(jobcmd, { detach = true })
+    vim.defer_fn(function() vim.fn.jobstop(job) end, 5000)
+end
+
+-- Clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
-keymap.set("n", "<leader>ss", ":source %<CR>", { desc = "Reload neovim config" }) -- reload nevom configurations quickly
 
--- increment/decremente numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+-- Reload Neovim config
+keymap.set("n", "<leader>ss", ":source %<CR>", { desc = "Reload neovim config" })
 
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertivically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make split equal size" }) -- make split window equal size
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+-- Increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
--- tabs
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) -- go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previos tab" }) -- go to previous tab
-keymap.set("n", "<leader>tg", "<cmd>tabnew %<CR>", { desc = "Open current buffere in new tab" }) -- open current file in new tah
+-- Window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>=", "<C-w>=", { desc = "Make split equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" }) -- paste from Clipboard raw, withou triggering any formatting or other text manipulations
+-- Tabs
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tg", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+
+-- Paste from clipboard
+keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
+
+-- Open URL under cursor in default browser
+keymap.set("n", "<leader>u", function()
+    open_external(vim.fn.expand("<cfile>"))
+end, { desc = "Open URL under cursor" })
