177a04d56febeb7a04ec65f3eb4cd8ae981a73c2
testusr <smeo@gmx.de>
2025-09-13 06:37:59 +0200

    start neogit only if in a git repo

--- nvim/.config/nvim/lua/user/plugins/neogit.lua
+++ nvim/.config/nvim/lua/user/plugins/neogit.lua
@@ -1,40 +1,49 @@
 return {
-	{
-		"NeogitOrg/neogit",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"sindrets/diffview.nvim",
-			-- Only one of these is needed.
-			"nvim-telescope/telescope.nvim", -- optional
-			"ibhagwan/fzf-lua", -- optional
-			"echasnovski/mini.pick", -- optional
-		},
-		config = function()
-			-- Setup Neogit
-			local neogit = require("neogit")
-			neogit.setup({
-				integrations = {
-					diffview = true, -- Enable Diffview integration
-				},
-				signs = {
-					section = { ">", "v" }, -- Customize signs
-					item = { ">", "v" },
-				},
-				disable_commit_confirmation = false, -- Show commit confirmation
-				auto_refresh = true, -- Auto-refresh status
-			})
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+    },
+    cmd = "Neogit",
+    keys = {
+      { "<leader>gn", "<cmd>Neogit<CR>", desc = "Open Neogit" },
+    },
 
-			-- Register with which-key
-			local wk = require("which-key")
-			wk.register({
-				gn = { "<cmd>Neogit<CR>", "Open Neogit" },
-			}, {
-				prefix = "<leader>",
-				mode = "n",
-				silent = true,
-				noremap = true,
-				nowait = true,
-			})
-		end,
-	},
+    -- Only enable if we're inside a git repo
+    cond = function()
+      vim.fn.system("git rev-parse --is-inside-work-tree")
+      return vim.v.shell_error == 0
+    end,
+
+    config = function()
+      local neogit = require("neogit")
+      neogit.setup({
+        integrations = {
+          diffview = true,
+        },
+        signs = {
+          section = { ">", "v" },
+          item = { ">", "v" },
+        },
+        disable_commit_confirmation = false,
+        auto_refresh = true,
+      })
+
+      local wk = require("which-key")
+      wk.register({
+        gn = { "<cmd>Neogit<CR>", "Open Neogit" },
+      }, {
+        prefix = "<leader>",
+        mode = "n",
+        silent = true,
+        noremap = true,
+        nowait = true,
+      })
+    end,
+  },
 }
+
